5c4fd9018b5f791e96fa2626f19186b1870c4189
Sara-Gavi <sara.gavilan.castro@gmail.com>
2024-10-06 19:17:17 +0200

    Setup Express server and MongoDB connection in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+const express = require("express");
+const connectDB = require("./utils/database"); // Importa la función de conexión a MongoDB
+
+const server = express();
+
+// Conectar a MongoDB
+connectDB();
+
+// Mensaje de prueba para verificar que el servidor funciona
 console.log("Hola que tal");
+
+// Iniciar el servidor en el puerto 5001
+const PORT = 5001;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
